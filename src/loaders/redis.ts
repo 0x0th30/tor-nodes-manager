a2c713d964d102169de2f5765a2e32667360b9f8
theo silva <theodorotheodore@gmail.com>
2023-02-24 04:29:58 -0300

    build: removes REdis auth

--- src/loaders/redis.ts
+++ src/loaders/redis.ts
@@ -14,8 +14,9 @@ redisClient.on('ready', () => {
   logger.info('Redis connection has been successfully established!');
 });
 
-redisClient.on('error', () => {
-  logger.error(`Redis connection has been failed. Trying again in ${TIMEOUT_IN_MS}ms...`);
+redisClient.on('error', (error) => {
+  logger.error(`Redis connection has been failed: ${error}.`
+    + ` Trying again in ${TIMEOUT_IN_MS}ms...`);
 });
 
 export { redisClient };
