5241beb693282a1a4b2aab4cdf89763c3b2680b9
theo silva <theodorotheodore@gmail.com>
2023-02-28 06:52:42 -0300

    fix(use-cases/get-filtered-ips): improves use-case logging

--- src/use-cases/get-filtered-ips.ts
+++ src/use-cases/get-filtered-ips.ts
@@ -1,5 +1,6 @@
 import { GetAllIps } from '@use-cases/get-all-ips';
 import { GetBannedIps } from '@use-cases/get-banned-ips';
+import { logger } from '@loaders/logger';
 
 interface GetFilteredIpsResponse {
   success: boolean,
@@ -22,8 +23,11 @@ class GetFilteredIps {
   }
 
   public async execute(): Promise<GetFilteredIpsResponse> {
+    logger.info('Initializing "get-filtered-ips" use-case/service...');
+
     const response: GetFilteredIpsResponse = { success: false };
 
+    logger.info('Getting all IPs...');
     const getAllIpsResponse = await this.getAllIps.execute();
     if (!getAllIpsResponse.success || !getAllIpsResponse.data) {
       response.success = false;
@@ -32,6 +36,7 @@ class GetFilteredIps {
       return response;
     }
 
+    logger.info('Getting banned IPs...');
     const getBannedIpsResponse = await this.getBannedIps.execute();
     if (!getBannedIpsResponse.success || !getBannedIpsResponse.data) {
       response.success = false;
@@ -44,12 +49,14 @@ class GetFilteredIps {
     const bannedIps = getBannedIpsResponse.data.addresses;
     let bannedIpsCount = 0;
 
+    logger.info('Removing banned IPs from listing...');
     const filteredIps: string[] = [];
     allIps.forEach((ip) => {
       if (!bannedIps.includes(ip)) filteredIps.push(ip);
       else bannedIpsCount += 1;
     });
 
+    logger.info('All banned IPs was successfully hidden.');
     response.success = true;
     response.data = {
       results: allIps.length,
@@ -57,6 +64,7 @@ class GetFilteredIps {
       addresses: filteredIps,
     };
 
+    logger.info('Finishing "get-filtered-ips" use-case/service.');
     return response;
   }
 }
