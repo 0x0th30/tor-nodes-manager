5848daef236f87e066f23f222483e829f11bbb7b
theo silva <theodorotheodore@gmail.com>
2023-03-21 05:38:53 -0300

    test: remove useless code from sketched file

--- tests/use-cases/get-filtered-ips.spec.ts
+++ tests/use-cases/get-filtered-ips.spec.ts
@@ -1,14 +1,3 @@
-import { GetFilteredIps } from '@use-cases/get-filtered-ips';
-import { GetAllIps } from '@use-cases/get-all-ips';
-import { GetBannedIps } from '@use-cases/get-banned-ips';
-import { GetAllIpsMock } from '@mocks/get-all-ips';
-import { GetBannedIpsMock } from '@mocks/get-banned-ips';
-
-const GetFilteredIpsSUT = new GetFilteredIps(
-  GetAllIpsMock as unknown as GetAllIps,
-  GetBannedIpsMock as unknown as GetBannedIps,
-);
-
 describe('"GetFilteredIps" class', () => {
   describe('(public) "execute" method', () => {
     it.todo('should call "getAllIps.execute" to get all IPs from sources');
