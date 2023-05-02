8d3695f7f82c137262056e359bffc560b8855d27
theo silva <theodorotheodore@gmail.com>
2023-05-02 04:23:22 -0300

    refactor: move server logic to "server.ts" an d startup code to "main.ts"

---
+++ app/src/api/server.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import { router } from '@api/routes';
+import { logger } from '@loaders/logger';
+
+export class Server {
+  private app = express();
+
+  private host = process.env['HOST'] || 'localhost';
+
+  private port = process.env['PORT'] || 3000;
+
+  private loadServerConfigs(): void {
+    logger.info('Setting up server configs...');
+    this.app.use(express.json());
+    this.app.use(router);
+  }
+
+  private bindServer(): void {
+    logger.info(`Binding server on specified host/port (${this.host}:${this.port})...`);
+    this.app.listen(this.port, () => {
+      logger.info(`Running application at http://${this.host}:${this.port}/`);
+    });
+  }
+
+  public start(): void {
+    logger.info('Starting HTTP server...');
+    this.loadServerConfigs();
+    this.bindServer();
+  }
+}
