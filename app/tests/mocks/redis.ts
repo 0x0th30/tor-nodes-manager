236825ee5b55dbdce187ea84e809aebe703a1c2b
theo silva <theodorotheodore@gmail.com>
2023-06-28 06:02:18 -0300

    test(get-all-ips): include missing tests

--- app/tests/mocks/redis.ts
+++ app/tests/mocks/redis.ts
@@ -2,6 +2,5 @@ import { redisClient } from '@loaders/redis';
 
 export const RedisClientMock = {
   lRange: jest.spyOn(redisClient, 'lRange'),
-  rPush: jest.spyOn(redisClient, 'rPush'),
-  expire: jest.spyOn(redisClient, 'expire'),
+  ttl: jest.spyOn(redisClient, 'ttl'),
 };
--- app/tests/use-cases/get-all-ips.spec.ts
+++ app/tests/use-cases/get-all-ips.spec.ts
@@ -4,6 +4,7 @@ import { GetAllIpsMock } from '@mocks/get-all-ips';
 import { RedisClientMock } from '@mocks/redis';
 import { RabbitMQMock } from '@mocks/rabbitmq';
 import { RabbitMQ } from '@loaders/rabbitmq';
+import { ProcessingNodeList } from '@errors/node-list-source-error';
 
 const GetAllIpsSUT = new GetAllIps(
   RedisClientMock as unknown as RedisClientType,
@@ -38,6 +39,76 @@ describe('"GetAllIps" class', () => {
           success: false,
           error: new Error('foo bar'),
         });
+      });
+    });
+  });
+  describe('(private) "getTorNodes" method', () => {
+    it('should call "RedisClientMock.lRange" search by IPs in Redis', () => {
+      GetAllIpsMock.getTorNodes.mockRestore();
+      RedisClientMock.lRange.mockResolvedValue(['4.4.4.4']);
+      RedisClientMock.ttl.mockResolvedValue(901);
+      RabbitMQMock.getQueueSize.mockResolvedValue(0);
+      RabbitMQMock.sendMessageToQueue.mockResolvedValue();
+
+      (GetAllIpsSUT as any).getTorNodes().then(() => {
+        expect(RedisClientMock.lRange).toBeCalled();
+      });
+    });
+    it('should call "RedisClientMock.ttl" get IPs TTL in Redis', () => {
+      GetAllIpsMock.getTorNodes.mockRestore();
+      RedisClientMock.lRange.mockResolvedValue(['4.4.4.4']);
+      RedisClientMock.ttl.mockResolvedValue(901);
+      RabbitMQMock.getQueueSize.mockResolvedValue(0);
+      RabbitMQMock.sendMessageToQueue.mockResolvedValue();
+
+      (GetAllIpsSUT as any).getTorNodes().then(() => {
+        expect(RedisClientMock.ttl).toBeCalled();
+      });
+    });
+    it('should call "RabbitMQMock.getQueueSize" to check queue size in RabbitMQ', () => {
+      GetAllIpsMock.getTorNodes.mockRestore();
+      RedisClientMock.lRange.mockResolvedValue(['4.4.4.4']);
+      RedisClientMock.ttl.mockResolvedValue(901);
+      RabbitMQMock.getQueueSize.mockResolvedValue(0);
+      RabbitMQMock.sendMessageToQueue.mockResolvedValue();
+
+      (GetAllIpsSUT as any).getTorNodes().then(() => {
+        expect(RabbitMQMock.getQueueSize).toBeCalled();
+      });
+    });
+    it('should return IPs if exists in Redis and TTL was bigger than 15 minutes', () => {
+      GetAllIpsMock.getTorNodes.mockRestore();
+      RedisClientMock.lRange.mockResolvedValue(['4.4.4.4']);
+      RedisClientMock.ttl.mockResolvedValue(901); // 900s = 15min
+      RabbitMQMock.getQueueSize.mockResolvedValue(0);
+      RabbitMQMock.sendMessageToQueue.mockResolvedValue();
+
+      (GetAllIpsSUT as any).getTorNodes().then((response: Array<string>) => {
+        expect(response).toEqual(['4.4.4.4']);
+      });
+    });
+    it('should call "RabbitMQClient.sendMessageToQueue" if exists in Redis but TTL was'
+        + ' less than 15 minutes', () => {
+      GetAllIpsMock.getTorNodes.mockRestore();
+      RedisClientMock.lRange.mockResolvedValue(['4.4.4.4']);
+      RedisClientMock.ttl.mockResolvedValue(899); // 900s = 15min
+      RabbitMQMock.getQueueSize.mockResolvedValue(0);
+      RabbitMQMock.sendMessageToQueue.mockResolvedValue();
+
+      (GetAllIpsSUT as any).getTorNodes().catch(() => {
+        expect(RabbitMQMock.sendMessageToQueue).toBeCalled();
+      });
+    });
+    it('should throw "ProcessingNodeList" if IPs already was send to be processed'
+        + ' in queue', () => {
+      GetAllIpsMock.getTorNodes.mockRestore();
+      RedisClientMock.lRange.mockResolvedValue(['4.4.4.4']);
+      RedisClientMock.ttl.mockResolvedValue(899); // 900s = 15min
+      RabbitMQMock.getQueueSize.mockResolvedValue(0);
+      RabbitMQMock.sendMessageToQueue.mockResolvedValue();
+
+      (GetAllIpsSUT as any).getTorNodes().catch((error: any) => {
+        expect(error).toEqual(new ProcessingNodeList());
       });
     });
   });
