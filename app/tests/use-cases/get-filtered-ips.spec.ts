e509c706b6574b17fd615d0aebc2e1e30669ee83
theo silva <theodorotheodore@gmail.com>
2023-06-28 06:17:55 -0300

    test(get-filtered-ips): include missing tests

--- app/tests/use-cases/get-filtered-ips.spec.ts
+++ app/tests/use-cases/get-filtered-ips.spec.ts
@@ -1,6 +1,88 @@
+import { GetAllIpsMock } from '@mocks/get-all-ips';
+import { GetBannedIpsMock } from '@mocks/get-banned-ips';
+import { GetFilteredIps } from '@use-cases/get-filtered-ips/get-filtered-ips.business';
+import { GetAllIps } from '@use-cases/get-all-ips/get-all-ips.business';
+import { GetBannedIps } from '@use-cases/get-banned-ips/get-banned-ips.business';
+
+const GetFilteredIpsSUT = new GetFilteredIps(
+  GetAllIpsMock as unknown as GetAllIps,
+  GetBannedIpsMock as unknown as GetBannedIps,
+);
+
 describe('"GetFilteredIps" class', () => {
   describe('(public) "execute" method', () => {
-    it.todo('should call "getAllIps.execute" to get all IPs from sources');
-    it.todo('should call "getBannedIps.execute" to get all banned IPs');
+    it('should call "GetAllIps.execute" to get all IPs from sources', () => {
+      GetAllIpsMock.execute.mockResolvedValue({
+        success: true,
+        data: { results: 2, addresses: ['4.4.4.4', '8.8.8.8'] },
+      });
+      GetBannedIpsMock.execute.mockResolvedValue({
+        success: true,
+        data: { addresses: ['4.4.4.4'] },
+      });
+
+      GetFilteredIpsSUT.execute().then(() => {
+        expect(GetAllIpsMock.execute).toBeCalled();
+      });
+    });
+    it('should call "GetBannedIps.execute" to get all banned IPs', () => {
+      GetAllIpsMock.execute.mockResolvedValue({
+        success: true,
+        data: { results: 2, addresses: ['4.4.4.4', '8.8.8.8'] },
+      });
+      GetBannedIpsMock.execute.mockResolvedValue({
+        success: true,
+        data: { addresses: ['4.4.4.4'] },
+      });
+
+      GetFilteredIpsSUT.execute().then(() => {
+        expect(GetBannedIpsMock.execute).toBeCalled();
+      });
+    });
+    it('should replicate "GetAllIps.execute" error if exists', () => {
+      GetAllIpsMock.execute.mockResolvedValue({
+        success: false,
+        error: new Error('foo bar'),
+      });
+      GetBannedIpsMock.execute.mockResolvedValue({
+        success: true,
+        data: { addresses: ['4.4.4.4'] },
+      });
+
+      GetFilteredIpsSUT.execute().then((response) => {
+        expect(response).toEqual({ success: false, error: new Error('foo bar') });
+      });
+    });
+    it('should replicate "GetBannedIps.execute" error if exists', () => {
+      GetAllIpsMock.execute.mockResolvedValue({
+        success: true,
+        data: { results: 2, addresses: ['4.4.4.4', '8.8.8.8'] },
+      });
+      GetBannedIpsMock.execute.mockResolvedValue({
+        success: false,
+        error: new Error('foo bar'),
+      });
+
+      GetFilteredIpsSUT.execute().then((response) => {
+        expect(response).toEqual({ success: false, error: new Error('foo bar') });
+      });
+    });
+    it('should remove banned IPs from listing', () => {
+      GetAllIpsMock.execute.mockResolvedValue({
+        success: true,
+        data: { results: 2, addresses: ['4.4.4.4', '8.8.8.8'] },
+      });
+      GetBannedIpsMock.execute.mockResolvedValue({
+        success: true,
+        data: { addresses: ['4.4.4.4'] },
+      });
+
+      GetFilteredIpsSUT.execute().then((response) => {
+        expect(response).toEqual({
+          success: true,
+          data: { results: 2, bannedIps: 1, addresses: ['8.8.8.8'] },
+        });
+      });
+    });
   });
 });
